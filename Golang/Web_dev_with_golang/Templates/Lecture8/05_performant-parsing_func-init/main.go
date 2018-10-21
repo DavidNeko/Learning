9634da32dde1caa939f013bb75333c2fa0e30e40
David Wang <davidandneko233@gmail.com>
2018-10-21 16:13:14 +0800

    Updated Udemy:WDWG learning note of lecture 8

---
+++ Golang/Web_dev_with_golang/Templates/Lecture8/05_performant-parsing_func-init/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	// Must handles error checking for us
+	// We use it to avoid error checking
+}
+
+func main() {
+	err := tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
